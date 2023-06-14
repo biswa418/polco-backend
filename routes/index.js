da38ee19820f2ae96a65a2ce8b23bc791c6bc92e
Biswajeet Sutar <biswajeetsutar418@gmail.com>
2023-06-14 07:19:20 +0530

    user signup/signin handle

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const router = express.Router();
 
+const userController = require('../controllers/userController');
+
 router.get('/', function (req, res) {
     return res.json({ message: "Hello" })
 });
 
+router.get('/signin', userController.signin);
 
 module.exports = router;
